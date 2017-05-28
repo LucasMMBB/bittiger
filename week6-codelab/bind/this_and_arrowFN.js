c8eb0972722a350bc330021d9c1f4357dcbcd99c
LucasMMBB <maoxummbb@gmail.com>
2017-05-27 20:19:42 -0400

    update this and arrowFN practice

--- week6-codelab/bind/this_and_arrowFN.js
+++ week6-codelab/bind/this_and_arrowFN.js
@@ -1,21 +1,41 @@
+/*
+	Arrow function get the parent variable for this.
+	ES5 function get the current variable for this
+*/
+
 var a = 37;
 
 var obj1 = {
-	a:39,
-	f: ()=>{
-		cosole.log('f', this.a);
-	},
-	ff: function(){
-		function f1(){
-			console.log('f1', this.a);
-		}
-		f1();
+    a: 39,
+    f: () => {
+        console.log('f', this.a); // 39
+    },
+    ff: function() {
+        function f1() {
+            console.log('f1', this.a);
+        }
+        f1();
 
-		
-	},
-	fff:function(){}
+        (() => {
+            console.log('f2', this.a);
+        })(); 
+    },
+    fff: function() {
+        function f1() {
+            var a = 41;
+            function f2() {
+                console.log('f2', this.a);
+            }
+            f2();
+
+            (() => {
+                console.log('f2', this.a);
+            })();
+        }
+        f1();
+    }
 }
 
-obj1.f();
-obj1.ff();
-obj1.fff();
+obj1.f(); // 37
+obj1.ff();// 37 39
+obj1.fff();// 37 37
