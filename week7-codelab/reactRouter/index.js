c5cda5107020d17244ef90f5c9bbe9d4d4ed4bad
LucasMMBB <maoxummbb@gmail.com>
2017-05-30 16:38:05 -0400

    fixed some error

--- week7-codelab/reactRouter/index.js
+++ week7-codelab/reactRouter/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { render } from 'react-dom'
 import App from './modules/App'
-import { Router, Route, hashHistory} from 'react-router'
+import { Router, Route, browserHistory} from 'react-router'
 
 // import other components and insert them into 
 import About from './modules/About'
@@ -9,7 +9,7 @@ import Repos from './modules/Repos'
 import Repo from './modules/Repo'
 
 render((
-	<Router history={hashHistory}>
+	<Router history={browserHistory}>
 		<Route path="/" component={App}>
 			<Route path="/repos" component={Repos}></Route>
 			<Route path="repos/:userName/:repoName" component={Repo}></Route>
