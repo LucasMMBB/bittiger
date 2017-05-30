73f052f0d3357805b64ea2e82c194aa3a20b0c19
LucasMMBB <maoxummbb@gmail.com>
2017-05-30 13:35:56 -0400

    finish half reactRouter tutorial

---
+++ week7-codelab/reactRouter/index.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { render } from 'react-dom'
+import App from './modules/App'
+import { Router, Route, hashHistory} from 'react-router'
+
+// import other components and insert them into 
+import About from './modules/About'
+import Repos from './modules/Repos'
+import Repo from './modules/Repo'
+
+render((
+	<Router history={hashHistory}>
+		<Route path="/" component={App}>
+			<Route path="/repos" component={Repos}></Route>
+			<Route path="repos/:userName/:repoName" component={Repo}></Route>
+			<Route path="/about" component={About}></Route>	
+		</Route>
+	</Router>
+), document.getElementById('app'))
---
+++ week7-codelab/reactRouter/modules/App.js
@@ -0,0 +1,22 @@
+// modules/App.js
+import React from 'react'
+import { Link } from 'react-router'
+import NavLink from './NavLink'
+
+
+export default React.createClass({
+  render() {
+    return (
+    	<div>
+    		<h1>App/Main Component</h1>
+    		<ul role="nav">
+    			<li><NavLink to="/about">About</NavLink></li>
+    			<li><NavLink to="/repos">Repos</NavLink></li>
+    		</ul>
+
+    		{this.props.children}
+    	
+    	</div>
+    )
+  }
+})
