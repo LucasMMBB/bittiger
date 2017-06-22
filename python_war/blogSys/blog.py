eefdb0f251b62d25b6a2adac13abbd73218826ee
LucasMMBB <maoxummbb@gmail.com>
2017-06-22 01:30:08 -0400

    init index page and front logic file in blog.py

--- python_war/blogSys/blog.py
+++ python_war/blogSys/blog.py
@@ -1,9 +1,20 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
+# - index page
 @app.route('/')
 def index():
-	return 'Hello from Oworld'
+	company = 'Fuckyou'
+	nums = [1,2,3,4,5]
+	blogs = [
+				{"id":1,"content":"how are you"},
+				{"id":2, "content": "i am fine"}
+			]
+	return	render_template('index.html.jinja',
+		cname = company,
+		nums = nums,
+		blogs = blogs)
 
 app.run(debug = True)
