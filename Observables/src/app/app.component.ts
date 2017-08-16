1196f3980e9fe2d5d0e1b84c501d7e9bc16b8d39
LucasMMBB <maoxummbb@gmail.com>
2017-08-15 22:01:56 -0400

    added more to observable tutorial

--- Observables/src/app/app.component.ts
+++ Observables/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Rx';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,41 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+	title = 'app';
+	
+	ngOnInit(): void {
+		let promise = new Promise(resolve => {
+			console.log('promise execution');
+			setTimeout(()=>{
+				resolve('promise resolved');
+			}, 2000);
+		});
+
+		promise.then(value => {
+			console.log(value);
+		});
+
+		let stream = new Observable(observer => {
+			console.log('observable execution');
+			observer.next(1);
+			observer.next(2);
+			let timeout = setTimeout(()=>{
+				observer.next('observer next value');
+			}, 2000);
+			observer.next(3);
+		
+			return function unsubscribe(){
+				clearTimeout(timeout);
+			}
+		});
+
+		let subscription = stream.subscribe(value => {
+			console.log(value);
+		});
+
+		setTimeout(()=>{
+			subscription.unsubscribe();
+		}, 1000);
+	}// ngOnInit ends
+
 }
